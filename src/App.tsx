0197fc58ac9f1e573f77bb77ff57d70f01ca2fb0
SieR-VR <me@sier-vr.dev>
2026-02-08 05:41:29 +0900

    feat/ui: reset and init reactflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,80 @@
-import "./App.css";
-import { AudioMultiplexer } from "./components/audio-multiplexer";
+import { useCallback, useState } from "react";
+import {
+  ReactFlow,
+  applyEdgeChanges,
+  applyNodeChanges,
+  addEdge,
+  Background,
+  BackgroundVariant,
+  MiniMap,
+  Position,
+  Panel,
+} from "@xyflow/react";
+
+import { invoke } from "@tauri-apps/api/core";
+
+import "@xyflow/react/dist/style.css";
+
+const initialNodes = [
+  {
+    id: "n1",
+    position: { x: 0, y: 0 },
+    data: { label: "Node 1" },
+    sourcePosition: Position.Right,
+    targetPosition: Position.Left,
+  },
+  {
+    id: "n2",
+    position: { x: 0, y: 100 },
+    data: { label: "Node 2" },
+    sourcePosition: Position.Right,
+    targetPosition: Position.Left,
+  },
+];
+const initialEdges = [{ id: "n1-n2", source: "n1", target: "n2" }];
 
 function App() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  const onConnect = useCallback(
+    (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    [],
+  );
+
   return (
-    <main>
-      <AudioMultiplexer />
-    </main>
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      fitView
+    >
+      <Background color="black" variant={BackgroundVariant.Dots} />
+      <MiniMap nodeColor={() => "blue"} nodeStrokeWidth={3} zoomable pannable />
+      <Panel position="bottom-center">
+        <button
+          onClick={() => {
+            console.log("fetching audio devices...");
+            invoke("get_audio_devices").then((devices) => console.log(devices));
+          }}
+          className="border left-5 top-5 z-10"
+        >
+          Get Audio Devices
+        </button>
+      </Panel>
+    </ReactFlow>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,7 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async () => ({
   plugins: [react(), tailwindcss()],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent Vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
