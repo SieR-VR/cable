ea350bbe3f3d3dcb25513716b9de829d1c5347a1
SieR-VR <me@sier-vr.dev>
2026-02-10 22:36:52 +0900

    feat: add deps

--- crates/tauri/src/lib.rs
+++ crates/tauri/src/lib.rs
@@ -1,108 +1,74 @@
-use std::sync::Mutex;
-use tauri::{Builder, Manager, State};
+use cpal::traits::{DeviceTrait, HostTrait};
+use tauri::Builder;
 
-use windows::core::PWSTR;
-use windows::Win32::Devices::FunctionDiscovery::PKEY_Device_FriendlyName;
-use windows::Win32::Media::Audio::{
-  IAudioCaptureClient, IAudioClient, IMMDeviceEnumerator, MMDeviceEnumerator,
-  AUDCLNT_SHAREMODE_SHARED, AUDCLNT_STREAMFLAGS_LOOPBACK,
-};
-use windows::Win32::System::Com::StructuredStorage::PropVariantToBSTR;
-use windows::Win32::System::Com::{CoCreateInstance, CLSCTX_ALL, STGM_READ};
+struct AppData {}
 
-struct AppData {
-  devices: Option<Vec<AudioDevice>>,
-}
-
-#[derive(Clone)]
+#[derive(Debug, Clone, serde::Serialize)]
 struct AudioDevice {
-  friendly_name: String,
-  id: Vec<u16>,
+  id: String,
+  readable_name: String,
+  descriptions: Vec<String>,
 }
 
 #[tauri::command]
-fn get_audio_devices(state: State<'_, Mutex<AppData>>) -> Vec<String> {
-  let mut state = state.lock().unwrap();
-  if let Some(devices) = &state.devices {
-    return devices.iter().map(|d| d.friendly_name.clone()).collect();
-  }
+fn get_audio_hosts() -> Vec<String> {
+  let available_hosts = cpal::available_hosts();
+  println!("Available audio hosts: {:?}", available_hosts);
 
-  unsafe {
-    let enumerator: IMMDeviceEnumerator =
-      CoCreateInstance(&MMDeviceEnumerator, None, CLSCTX_ALL).unwrap();
-
-    let device_collection = enumerator
-      .EnumAudioEndpoints(
-        windows::Win32::Media::Audio::eRender,
-        windows::Win32::Media::Audio::DEVICE_STATE_ACTIVE,
-      )
-      .unwrap();
-    let count = device_collection.GetCount().unwrap();
-
-    let mut devices: Vec<AudioDevice> = Vec::new();
-    for i in 0..count {
-      let device = device_collection.Item(i).unwrap();
-      let device_name_propvariant = device
-        .OpenPropertyStore(STGM_READ)
-        .unwrap()
-        .GetValue(&PKEY_Device_FriendlyName)
-        .unwrap();
-      let device_name = PropVariantToBSTR(&device_name_propvariant)
-        .unwrap()
-        .to_string();
-      let device_id = device.GetId().unwrap();
-
-      devices.push(AudioDevice {
-        friendly_name: device_name,
-        id: device_id.as_wide().to_vec(),
-      });
-    }
-
-    state.devices = Some(devices.clone());
-    devices.iter().map(|d| d.friendly_name.clone()).collect()
-  }
+  available_hosts.iter().map(|h| format!("{:?}", h)).collect()
 }
 
 #[tauri::command]
-fn capture_audio(
-  state: State<'_, Mutex<AppData>>,
-  device_index: usize,
-) -> Result<(), windows::core::Error> {
-  let state = state.lock().unwrap();
-  let devices = state.devices.as_ref().expect("Audio devices not loaded");
-  let device = &devices[device_index];
-  let device_id = PWSTR::from_raw(device.id.as_ptr() as *mut u16);
+fn get_audio_devices(host: String) -> (Vec<AudioDevice>, Vec<AudioDevice>) {
+  let host_id = match cpal::available_hosts()
+    .into_iter()
+    .find(|h| format!("{:?}", h) == host)
+  {
+    Some(h) => h,
+    None => return (Vec::new(), Vec::new()),
+  };
 
-  unsafe {
-    let enumerator: IMMDeviceEnumerator = CoCreateInstance(&MMDeviceEnumerator, None, CLSCTX_ALL)?;
-    let device = enumerator.GetDevice(device_id)?;
+  let host = cpal::host_from_id(host_id).unwrap();
+  let input_devices = host
+    .input_devices()
+    .unwrap()
+    .map(|d| {
+      let description = d.description().unwrap();
 
-    let audio_client: IAudioClient = device.Activate::<IAudioClient>(CLSCTX_ALL, None).unwrap();
+      AudioDevice {
+        id: d.id().unwrap().to_string(),
+        readable_name: description.name().to_string(),
+        descriptions: description.extended().to_vec(),
+      }
+    })
+    .collect();
+  let output_devices = host
+    .output_devices()
+    .unwrap()
+    .map(|d| {
+      let description = d.description().unwrap();
 
-    audio_client.Initialize(
-      AUDCLNT_SHAREMODE_SHARED,
-      AUDCLNT_STREAMFLAGS_LOOPBACK,
-      10000000,
-      0,
-      audio_client.GetMixFormat()?,
-      None,
-    )?;
+      AudioDevice {
+        id: d.id().unwrap().to_string(),
+        readable_name: description.name().to_string(),
+        descriptions: description.extended().to_vec(),
+      }
+    })
+    .collect();
 
-    let capture_client = audio_client.GetService::<IAudioCaptureClient>()?;
-
-    Ok(())
-  }
+  println!(
+    "Input devices: {:?}, Output devices: {:?}",
+    input_devices, output_devices
+  );
+  (input_devices, output_devices)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   Builder::default()
-    .setup(|app| {
-      app.manage(AppData { devices: None });
-      Ok(())
-    })
     .plugin(tauri_plugin_opener::init())
-    .invoke_handler(tauri::generate_handler![get_audio_devices])
+    .manage(AppData {})
+    .invoke_handler(tauri::generate_handler![get_audio_hosts, get_audio_devices])
     .run(tauri::generate_context!())
     .unwrap();
 }
